70f968e366152adc0cc5eba1e00cad048adbab06
adenprince <45126168+adenprince@users.noreply.github.com>
2022-09-25 22:48:08 -0500

    Add code for reading body data and license from Azure-Kinect-Samples repository

--- Program.cs
+++ Program.cs
@@ -1,10 +1,51 @@
 ﻿namespace AzureKinectOSC
 {
+    using System;
+    using Microsoft.Azure.Kinect.BodyTracking;
+    using Microsoft.Azure.Kinect.Sensor;
+
     class Program
     {
         static void Main(string[] args)
         {
-            
+            // Open device
+            using (Device device = Device.Open()) {
+                device.StartCameras(new DeviceConfiguration() {
+                    CameraFPS = FPS.FPS30,
+                    ColorResolution = ColorResolution.Off,
+                    DepthMode = DepthMode.NFOV_Unbinned,
+                    WiredSyncMode = WiredSyncMode.Standalone,
+                });
+
+                var deviceCalibration = device.GetCalibration();
+
+                using (Tracker tracker = Tracker.Create(deviceCalibration, new TrackerConfiguration() { ProcessingMode = TrackerProcessingMode.Gpu, SensorOrientation = SensorOrientation.Default })) {
+                    while (true) {
+                        using (Capture sensorCapture = device.GetCapture()) {
+                            // Queue latest frame from the sensor
+                            tracker.EnqueueCapture(sensorCapture);
+                        }
+
+                        // Try getting latest tracker frame
+                        using (Frame frame = tracker.PopResult(TimeSpan.Zero, throwOnTimeout: false)) {
+                            if (frame != null) {
+                                Console.WriteLine($"{frame.NumberOfBodies} bodies detected");
+                                for (uint i = 0; i < frame.NumberOfBodies; ++i) {
+                                    var skeleton = frame.GetBodySkeleton(i);
+
+                                    for (int jointId = 0; jointId < (int)JointId.Count; ++jointId) {
+                                        var joint = skeleton.GetJoint(jointId);
+                                    }
+                                }
+                            }
+                        }
+
+                        if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape) {
+                            break;
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# azure-kinect-osc
+# Azure Kinect OSC
+This repository uses some code from the `csharp_3d_viewer` sample in the Microsoft [`Azure-Kinect-Samples` repository](https://github.com/microsoft/Azure-Kinect-Samples/tree/master/body-tracking-samples/csharp_3d_viewer), which has the following license:
+
+```
+    MIT License
+
+    Copyright (c) Microsoft Corporation. All rights reserved.
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE
+```
