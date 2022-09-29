ded0f010091dcd3ffeb736030c0d0aa8baa5e666
adenprince <45126168+adenprince@users.noreply.github.com>
2022-09-28 19:33:54 -0500

    Move some code in Main() to separate methods

--- Program.cs
+++ Program.cs
@@ -10,35 +10,16 @@
     {
         static void Main(string address = "127.0.0.1", int port = 12345, bool sendJointOrientation = false)
         {
-            IPAddress ipAddress = IPAddress.Parse(address);
-
-            Device device = null;
-            OscSender sender = new OscSender(ipAddress, port);
-
-            try {
-                try {
-                    device = Device.Open();
-                }
-                catch (Exception e) {
-                    Console.WriteLine($"Failed to open Azure Kinect device with exception message: {e.Message}");
-                    throw;
-                }
-
-                try {
-                    sender.Connect();
-                }
-                catch (Exception e) {
-                    Console.WriteLine($"Failed to connect to OSC sender with exception message: {e.Message}");
-                    throw;
-                }
-            }
-            catch (Exception) {
-                device?.Dispose();
+            if (!TryOscSenderConnect(address, port, out OscSender sender)) {
                 sender?.Dispose();
                 return;
             }
 
-            Console.WriteLine("here :)");
+            if (!TryDeviceOpen(out Device device)) {
+                sender?.Dispose();
+                device?.Dispose();
+                return;
+            }
 
             try {
                 device.StartCameras(new DeviceConfiguration() {
@@ -60,35 +41,7 @@
                         // Try getting latest tracker frame
                         using (Frame frame = tracker.PopResult(TimeSpan.Zero, throwOnTimeout: false)) {
                             if (frame != null) {
-                                Console.WriteLine($"{frame.NumberOfBodies} bodies detected");
-                                for (uint bodyId = 0; bodyId < frame.NumberOfBodies; ++bodyId) {
-                                    var skeleton = frame.GetBodySkeleton(bodyId);
-
-                                    for (int jointId = 0; jointId < (int)JointId.Count; ++jointId) {
-                                        var joint = skeleton.GetJoint(jointId);
-                                        string messageAddress = $"/bodies/{bodyId}/joints/{jointId}";
-                                        OscMessage message;
-
-                                        if (sendJointOrientation) {
-                                            message = new OscMessage(messageAddress,
-                                                joint.Position.X,
-                                                joint.Position.Y,
-                                                joint.Position.Z,
-                                                joint.Quaternion.X,
-                                                joint.Quaternion.Y,
-                                                joint.Quaternion.Z,
-                                                joint.Quaternion.W);
-                                        }
-                                        else {
-                                            message = new OscMessage(messageAddress,
-                                                joint.Position.X,
-                                                joint.Position.Y,
-                                                joint.Position.Z);
-                                        }
-
-                                        sender.Send(message);
-                                    }
-                                }
+                                SendBodyData(sender, frame, sendJointOrientation);
                             }
                         }
 
@@ -101,6 +54,66 @@
             finally {
                 device?.Dispose();
                 sender?.Dispose();
+            }
+        }
+
+        public static bool TryOscSenderConnect(string address, int port, out OscSender sender) {
+            sender = null;
+
+            try {
+                IPAddress ipAddress = IPAddress.Parse(address);
+                sender = new OscSender(ipAddress, port);
+                sender.Connect();
+                return true;
+            }
+            catch (Exception e) {
+                Console.WriteLine($"Failed to connect to OSC sender with exception message: {e.Message}");
+                return false;
+            }
+        }
+
+        public static bool TryDeviceOpen(out Device device) {
+            device = null;
+
+            try {
+                device = Device.Open();
+                return true;
+            }
+            catch (Exception e) {
+                Console.WriteLine($"Failed to open Azure Kinect device with exception message: {e.Message}");
+                return false;
+            }
+        }
+
+        public static void SendBodyData(OscSender sender, Frame frame, bool sendJointOrientation) {
+            Console.WriteLine($"{frame.NumberOfBodies} bodies detected");
+            for (uint bodyId = 0; bodyId < frame.NumberOfBodies; ++bodyId) {
+                var skeleton = frame.GetBodySkeleton(bodyId);
+
+                for (int jointId = 0; jointId < (int)JointId.Count; ++jointId) {
+                    var joint = skeleton.GetJoint(jointId);
+                    string messageAddress = $"/bodies/{bodyId}/joints/{jointId}";
+                    OscMessage message;
+
+                    if (sendJointOrientation) {
+                        message = new OscMessage(messageAddress,
+                            joint.Position.X,
+                            joint.Position.Y,
+                            joint.Position.Z,
+                            joint.Quaternion.X,
+                            joint.Quaternion.Y,
+                            joint.Quaternion.Z,
+                            joint.Quaternion.W);
+                    }
+                    else {
+                        message = new OscMessage(messageAddress,
+                            joint.Position.X,
+                            joint.Position.Y,
+                            joint.Position.Z);
+                    }
+
+                    sender.Send(message);
+                }
             }
         }
     }
