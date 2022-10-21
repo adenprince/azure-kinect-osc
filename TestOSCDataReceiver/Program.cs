95b24656f093688f61d87853a923f4b2a97f706c
adenprince <45126168+adenprince@users.noreply.github.com>
2022-10-20 21:04:50 -0500

    Add TestOSCDataReceiver project to AzureKinectOSC solution

---
+++ TestOSCDataReceiver/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace TestOSCDataReceiver {
+    using Rug.Osc;
+    using System;
+    using System.Net;
+
+    class Program {
+        static void Main(string[] args) {
+            IPAddress address = IPAddress.Parse("127.0.0.1");
+            int port = 12345;
+
+            using (OscReceiver receiver = new OscReceiver(address, port)) {
+                receiver.Connect();
+
+                while (true) {
+                    OscPacket receivedPacket = receiver.Receive();
+
+                    Console.WriteLine(receivedPacket);
+
+                    if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape) {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
